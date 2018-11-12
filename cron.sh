57e8c2a82bae78e4df715805942ac72eb113bdc8
Segfault42 <SegFault42@protonmail.com>
2018-11-12 21:10:02 +0000

    update SDFile

--- cron.sh
+++ cron.sh
@@ -1,5 +1,12 @@
 echo "Cron" >> /tmp/cron.log
 
+export DEVKITPRO=/opt/devkitpro
+export DEVKITARM=/opt/devkitpro/devkitARM
+export DEVKITPPC=/opt/devkitpro/devkitPPC
+export DEVKITA64=/opt/devkitpro/devkitA64
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin
+
+
 cd /home/ubuntu/go/src/github.com/SegFault42/YetAnotherSwitchSDFileManager > /tmp/cron.log
 go run main.go > /tmp/cron.log
 git add . > /tmp/cron.log
