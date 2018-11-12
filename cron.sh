7e78faeb19745b471474eab6526a754ceebd3f51
Segfault42 <SegFault42@protonmail.com>
2018-11-12 20:54:02 +0000

    update SDFile

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 echo "Cron" >> /tmp/cron.log
 
-cd go/src/github.com/SegFault42/YetAnotherSwitchSDFileManager > /tmp/cron.log
+cd /home/ubuntu/go/src/github.com/SegFault42/YetAnotherSwitchSDFileManager > /tmp/cron.log
 go run main.go > /tmp/cron.log
 git add . > /tmp/cron.log
 git commit -m "update SDFile" > /tmp/cron.log
