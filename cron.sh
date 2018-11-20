9d18df4d375d6797b7632c75435fc7140ed23d07
Segfault42 <SegFault42@protonmail.com>
2018-11-20 13:06:01 +0000

    update SDFile

--- cron.sh
+++ cron.sh
@@ -1,17 +1,20 @@
 #!/bin/zsh
 
-echo "Cron" >> /tmp/cron.log
+while true; do
+	echo "Cron" >> /tmp/cron.log
 
-export DEVKITPRO=/opt/devkitpro
-export DEVKITARM=/opt/devkitpro/devkitARM
-export DEVKITPPC=/opt/devkitpro/devkitPPC
-export DEVKITA64=/opt/devkitpro/devkitA64
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin
+	export DEVKITPRO=/opt/devkitpro
+	export DEVKITARM=/opt/devkitpro/devkitARM
+	export DEVKITPPC=/opt/devkitpro/devkitPPC
+	export DEVKITA64=/opt/devkitpro/devkitA64
+	export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin
 
 
-cd /home/ubuntu/go/src/github.com/SegFault42/YetAnotherSwitchSDFileManager
-/usr/local/go run main.go
-/usr/bin/git add .
-/usr/bin/git commit -m "update SDFile"
-/usr/bin/git push
+	cd /home/ubuntu/go/src/github.com/SegFault42/YetAnotherSwitchSDFileManager
+	/usr/local/go run main.go
+	/usr/bin/git add .
+	/usr/bin/git commit -m "update SDFile"
+	/usr/bin/git push
+	sleep(1);
+done
 
